ad6d5a9d6dd197118568d8f83c0034a38e41f00f
naja <naja.nadhirah@ionnex.com>
2020-08-18 19:15:19 +0800

    feat: create layout for sign up module

--- config/nuxt/webfontloader.js
+++ config/nuxt/webfontloader.js
@@ -1,3 +1,5 @@
+
+
 export default {
   google: {
     families: ['Montserrat:400,700,900&display=swap']
---
+++ pages/register.vue
@@ -0,0 +1,212 @@
+<template>
+  <v-container id="pages__register" class="fill-height" fluid>
+    <v-layout>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="5" md="5">
+          <v-card class="elevation-1 pa-3 border-radius-box secondary">
+            <alert-form-error :error-message="errorMessage" />
+            <v-card-text>
+              <div class="layout column align-center">
+                <img src="logo-black.png" height="50" width="auto" contain />
+                <h2
+                  class="flex text-xs-center text-black bold font-weight-black my-4"
+                >
+                  {{ $t("label.joinUs") }}
+                </h2>
+              </div>
+              <div>
+                <v-btn
+                  color="#3b5998"
+                  block
+                  dark
+                  large
+                  @click="loginWithFacebook"
+                  class="mt-5"
+                >
+                  <img
+                    width="auto"
+                    height="25px"
+                    class="pr-5"
+                    src="facebook.png"
+                  />{{ $t("label.loginWithFacebook") }}
+                </v-btn>
+                <v-btn
+                  block
+                  @click="loginWithGoogle"
+                  class="mt-5"
+                  large
+                  outlined
+                >
+                  <img
+                    width="auto"
+                    height="25px"
+                    class="pr-5"
+                    src="google.png"
+                  />{{ $t("label.loginWithGoogle") }}
+                </v-btn>
+              </div>
+              <div class="login-option__divider">
+                <div class="login-option__divider--wrapper"></div>
+                <h5 class="mb-0 py-2 text-center">{{ $t("label.or") }}</h5>
+                <div class="login-option__divider--wrapper"></div>
+              </div>
+
+              <v-form>
+                <v-text-field
+                  text--primary
+                  font-weigth-black
+                  hide-details
+                  class="my-2"
+                  text-grey
+                  single-line
+                  solo
+                  flat
+                  dense
+                  type="email"
+                  :label="$t('label.email')"
+                  :error-messages="formErrors ? formErrors.email : ''"
+                  v-model="formModel.email"
+                ></v-text-field>
+                <v-text-field
+                  hide-details
+                  class="my-2"
+                  single-line
+                  solo
+                  flat
+                  dense
+                  type="password"
+                  :label="$t('label.password')"
+                  :error-messages="formErrors ? formErrors.password : ''"
+                  v-model="formModel.password"
+                ></v-text-field>
+                <v-text-field
+                  hide-details
+                  class="my-2"
+                  single-line
+                  solo
+                  flat
+                  dense
+                  type="password"
+                  :label="$t('label.confirmPassword')"
+                  :error-messages="formErrors ? formErrors.password : ''"
+                  v-model="formModel.password"
+                ></v-text-field>
+                <v-text-field
+                  hide-details
+                  class="my-2"
+                  single-line
+                  solo
+                  flat
+                  dense
+                  type="email"
+                  :label="$t('label.referalCode')"
+                  :error-messages="formErrors ? formErrors.email : ''"
+                  v-model="formModel.email"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="flex-column text-right">
+              <v-btn class="default-button white--text" block @click="login">
+                {{ $t("label.signUp") }}
+              </v-btn>
+              <v-checkbox
+              hide-details
+                v-model="checkbox"
+                :rules="[v => !!v || 'You must agree to continue!']"
+                required
+                label="I agree with KOL Mall Terms and Condition"
+              ></v-checkbox>
+              <!-- <a href="#">
+                   {{ 'Terms and Condition' }}
+              </a> -->
+              <a
+                :href="localePath('forgotPassword')"
+                class="caption text-right mt-2"
+              >
+                Forgot Password?
+              </a>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AlertFormError from "@/components/widgets/alerts/AlertFormError";
+
+import formMixin from "@/mixins/form";
+
+export default {
+  auth: "guest",
+  layout: "auth",
+  mixins: [formMixin],
+  components: {
+    AlertFormError
+  },
+  asyncData() {
+    return {
+      formModel: {
+        type: 0,
+        email: "",
+        password: ""
+      }
+    };
+  },
+  beforeCreate() {
+    if (this.$route.hash) {
+      this.$store.commit("setOverlay", true);
+    }
+  },
+  methods: {
+    async login() {
+      this.$store.commit("setOverlay", true);
+
+      try {
+        await this.$auth.loginWith("local", {
+          data: this.formModel
+        });
+        this.clearPreviousError();
+        this.$router.push("/");
+      } catch (err) {
+        this.handleApiErrors(err);
+      } finally {
+        this.$store.commit("setOverlay", false);
+      }
+    },
+    async loginWithFacebook() {
+      this.$store.commit("setOverlay", true);
+
+      try {
+        await this.$auth.loginWith("facebook");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.register-option__divider {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  margin: 15px 0;
+}
+
+h5:before, 
+        h5:after { 
+            content: ""; 
+            flex: 1 1; 
+            border-bottom: 2px solid lightgray; 
+            margin: auto; 
+            padding: 0 20px;
+        } 
+         h5 { 
+            display: flex; 
+            flex-direction: row; 
+        }
+</style>
