538cf1f20183840606e78eaf941f0ce6d22bc03e
Amir Asyraf <amir.halim@ionnex.com>
2020-08-18 19:14:56 +0800

    fix: Disable text decoration for text ref link

--- pages/login.vue
+++ pages/login.vue
@@ -3,9 +3,9 @@
     <v-layout>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="6" md="5">
-          <v-card class="elevation-1 pa-3 radius secondary" >
+          <v-card class="elevation-1 pa-3 radius secondary">
             <alert-form-error :error-message="errorMessage" />
-            <v-card-text >
+            <v-card-text>
               <div class="layout column align-center">
                 <h1 class="flex text-center mt-4 mb-2 primary--text">
                   <v-img icon class="pl-1">
@@ -33,20 +33,39 @@
                   @click="loginWithFacebook"
                   class="mt-5"
                 >
-                   <img width="auto" height="25px" class="pr-5" src="../assets/Img/facebook.png"/>{{ $t("label.loginWithFacebook") }}
+                  <img
+                    width="auto"
+                    height="25px"
+                    class="pr-5"
+                    src="../assets/Img/facebook.png"
+                  />{{ $t("label.loginWithFacebook") }}
                 </v-btn>
-                <v-btn block  @click="loginWithGoogle" class="mt-5"  large outlined>
-                 <img width="auto" height="25px" class="pr-5" src="../assets/Img/google.png"/>{{ $t("label.loginWithGoogle") }}
+                <v-btn
+                  block
+                  @click="loginWithGoogle"
+                  class="mt-5"
+                  large
+                  outlined
+                >
+                  <img
+                    width="auto"
+                    height="25px"
+                    class="pr-5"
+                    src="../assets/Img/google.png"
+                  />{{ $t("label.loginWithGoogle") }}
                 </v-btn>
                 <v-row align="center" justify="center" class="mt-10 mb-10">
-                  <h4 class="subtitle font-weight-bold black--text text-center" :href="localePath('')">
-                    Dont't have an account?  
+                  <h4
+                    class="subtitle font-weight-bold black--text text-center"
+                    :href="localePath('')"
+                  >
+                    Dont't have an account?
                   </h4>
-                  <a href="#" class=" text-h4 ">
-                   Sign Up now
+                  <a href="#" class=" text-h4 pl-1 " style="text-decoration:none;">
+                    Sign Up Now
                   </a>
                 </v-row>
-                 <v-text-field
+                <v-text-field
                   type="email"
                   :label="$t('label.email')"
                   :error-messages="formErrors ? formErrors.email : ''"
@@ -61,24 +80,31 @@
                   type="password"
                   :label="$t('label.password')"
                   :error-messages="formErrors ? formErrors.password : ''"
-                   single-line
+                  single-line
                   flat
                   solo
-                   aria-autocomplete="on"
-                   hide-details
+                  aria-autocomplete="on"
+                  hide-details
                   class="mb-2"
                 ></v-text-field>
               </v-form>
             </v-card-text>
             <v-card-actions class="flex-column text-right">
-              <v-btn class="default-button text-white" block @click="login" large>
+              <v-btn
+                class="default-button text-white"
+                block
+                @click="login"
+                large
+              >
                 <h4 class="text-white text-h3">{{ $t("label.login") }}</h4>
               </v-btn>
               <a
                 :href="localePath('forgotPassword')"
-                class="caption text-right mt-5"
+                class="caption text-right mt-5 "
+                style="text-decoration:none;"
+                
               >
-               <h3> Forgot Password?</h3>
+                Forgot Password
               </a>
               <div class="login-option__divider">
                 <div class="login-option__divider--wrapper">
@@ -164,6 +190,6 @@ export default {
   }
 }
 .radius {
-  border-radius: 25px!important;
+  border-radius: 25px !important;
 }
 </style>
