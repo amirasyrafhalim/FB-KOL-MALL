edf2bfadedd268b33326329c02d85e213b884ecc
Amir Asyraf <amir.halim@ionnex.com>
2020-08-28 13:38:57 +0800

    fix: create campaign no error

--- components/pages/campaigns/FormBasic.vue
+++ components/pages/campaigns/FormBasic.vue
@@ -6,42 +6,11 @@
         :label="$t('label.name')"
         :error-messages="formErrors ? formErrors.name : ''"
       />
-      <!-- <v-autocomplete
-          :items="merchants"
-          :label="$t('menuTitle.facebookPage')"
-          :error-messages="formErrors ? formErrors.merchant_name : ''"
-          item-text="name"
-          @input.native="fetchItems($event.target.value)"
-        ></v-autocomplete -->
     </v-col>
     <v-col cols="12" md="7">
       <crud-form-action backPath="campaigns" :loading="loading" @submit="submit"
     /></v-col>
 
-    <!--<v-text-field-->
-    <!--v-model="formModel.stream_id"-->
-    <!--:label="$t('label.streamId')"-->
-    <!--:error-messages="formErrors ? formErrors.stream_id : ''"-->
-    <!--/>-->
-
-    <!--<v-text-field-->
-    <!--v-model="formModel.stream_start_at"-->
-    <!--:label="$t('label.streamStartAt')"-->
-    <!--:error-messages="formErrors ? formErrors.stream_start_at : ''"-->
-    <!--/>-->
-
-    <!--<v-text-field-->
-    <!--v-model="formModel.stream_end_at"-->
-    <!--:label="$t('label.streamEndAt')"-->
-    <!--:error-messages="formErrors ? formErrors.stream_end_at : ''"-->
-    <!--/>-->
-    <!-- <input-radio-group
-         :disabled="formModel.status === 2"
-         v-model="formModel.status"
-         :label="$t('label.status')"
-         :options="statusEnums"
-         :errorMessage="formErrors ? formErrors.status : ''"
-      /> -->
   </div>
 </template>
 
@@ -80,12 +49,7 @@ export default {
       }
     };
   },
-  computed: {
-    // merchants() {
-    //   // return this.$store.state.merchants.records;
-    //   return this.$store.dispatch(`merchants/fetchDetail`);
-    // }
-  },
+ 
   watch: {
     record(value) {
       if (value !== "undefined") {
@@ -94,18 +58,8 @@ export default {
       }
     }
   },
-  // created() {
-  //   // this.formModel.merchant_id = this.$auth.user.merchant.id;
-  //   this.fetchItems();
-  // },
-  
+ 
   methods: {
-
-    // fetchItems(value) {
-    //   this.$store.dispatch(`merchants/fetchDetail`, {
-    //     merchant_id: value
-    //   });
-    // },
     async submit() {
       this.loading = true;
 
