baecf025f6a24c9d6cb68ba3a82c76f9763e5e7c
Farhan <ahmad.farhan@globaltium.com>
2020-09-04 06:44:30 +0800

    add laravel echo package

--- config/nuxt/buildModules.js
+++ config/nuxt/buildModules.js
@@ -1,1 +1,11 @@
-export default ["@nuxtjs/dotenv", "@nuxtjs/vuetify"];
+export default [
+	'@nuxtjs/dotenv',
+	'@nuxtjs/vuetify',
+	[
+		'@nuxtjs/laravel-echo',
+		{
+			broadcaster: 'socket.io',
+			host: 'http://localhost:6001'
+		}
+	]
+];
---
+++ pages/videos/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Video Console</h1>
+    {{ record }}
+  </div>
+</template>
+
+<script>
+import getOneMixin from "@/mixins/getOne";
+
+export default {
+  name: "index",
+  mixins: [getOneMixin],
+  asyncData() {
+    return {
+      moduleName: "videos"
+    };
+  },
+};
+</script>
