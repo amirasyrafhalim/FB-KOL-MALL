770dbf7820b953ffcd07ef3d3dce6732e433dc22
Farhan <ahmad.farhan@globaltium.com>
2020-09-08 15:42:11 +0800

    Update main.vue and logo

--- layouts/main.vue
+++ layouts/main.vue
@@ -19,7 +19,9 @@
       <div id="content-overlay" />
 
       <!-- Navbar -->
+
       <template v-if="mainLayoutType === 'horizontal' && windowWidth >= 1200">
+        test
         <the-navbar-horizontal
           :navbarType="navbarType"
           :class="[
@@ -181,7 +183,8 @@ export default {
       navbarType: themeConfig.navbarType || "floating",
       navMenuItems,
       routerTransition: themeConfig.routerTransition || "none",
-      routeTitle: this.$route.meta.pageTitle,
+      routeTitle: "Dashboard",
+      vueAppClasses: [],
     };
   },
   watch: {
@@ -194,6 +197,12 @@ export default {
     },
     "$store.state.mainLayoutType"(val) {
       this.setNavMenuVisibility(val);
+    },
+    "$store.state.theme"(val) {
+      this.toggleClassInBody(val);
+    },
+    "$vs.rtl"(val) {
+      document.documentElement.setAttribute("dir", val ? "rtl" : "ltr");
     },
   },
   computed: {
@@ -263,14 +272,64 @@ export default {
         this.$store.commit("TOGGLE_IS_VERTICAL_NAV_MENU_ACTIVE", true);
       }
     },
+    toggleClassInBody(className) {
+      if (className === "dark") {
+        if (document.body.className.match("theme-semi-dark"))
+          document.body.classList.remove("theme-semi-dark");
+        document.body.classList.add("theme-dark");
+      } else if (className === "semi-dark") {
+        if (document.body.className.match("theme-dark"))
+          document.body.classList.remove("theme-dark");
+        document.body.classList.add("theme-semi-dark");
+      } else {
+        if (document.body.className.match("theme-dark"))
+          document.body.classList.remove("theme-dark");
+        if (document.body.className.match("theme-semi-dark"))
+          document.body.classList.remove("theme-semi-dark");
+      }
+    },
+    setAppClasses(classesStr) {
+      this.vueAppClasses.push(classesStr);
+    },
+    handleWindowResize() {
+      this.$store.commit("UPDATE_WINDOW_WIDTH", window.innerWidth);
+
+      // Set --vh property
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    },
+    handleScroll() {
+      this.$store.commit("UPDATE_WINDOW_SCROLL_Y", window.scrollY);
+    },
+  },
+  mounted() {
+    this.toggleClassInBody(themeConfig.theme);
+    this.$store.commit("UPDATE_WINDOW_WIDTH", window.innerWidth);
+
+    const vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
   },
   created() {
+    const dir = this.$vs.rtl ? "rtl" : "ltr";
+    document.documentElement.setAttribute("dir", dir);
+
+    window.addEventListener("resize", this.handleWindowResize);
+    window.addEventListener("scroll", this.handleScroll);
+
     const color =
       this.navbarColor === "#fff" && this.isThemeDark
         ? "#10163a"
         : this.navbarColor;
     this.updateNavbarColor(color);
+
     this.setNavMenuVisibility(this.$store.state.mainLayoutType);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleWindowResize);
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
