89c7c39017c22ba74a53a0251fb8dc646e22a903
nadiahsohaini <nadiahsohaini@gmail.com>
2020-09-09 14:51:14 +0800

    feat: campaigns module

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,26 +1,16 @@
-<!-- =========================================================================================
-  File Name: UserList.vue
-  Description: User List page
-  ----------------------------------------------------------------------------------------
-  Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-  Author: Pixinvent
-  Author URL: http://www.themeforest.net/user/pixinvent
-========================================================================================== -->
-
 <template>
 
   <div id="page-user-list">
-
     <div class="vx-card p-6">
-
       <div class="flex flex-wrap items-center">
 
         <!-- ITEMS PER PAGE -->
         <div class="flex-grow">
           <vs-dropdown vs-trigger-click class="cursor-pointer">
-            <div class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium">
+            <div
+              class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium">
               <span class="mr-2">{{ currentPage * paginationPageSize - (paginationPageSize - 1) }} - {{ records.length - currentPage * paginationPageSize > 0 ? currentPage * paginationPageSize : records.length }} of {{ records.length }}</span>
-              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4"/>
             </div>
             <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
             <vs-dropdown-menu>
@@ -42,51 +32,10 @@
         </div>
 
         <!-- TABLE ACTION COL-2: SEARCH & EXPORT AS CSV -->
-        <vs-input class="sm:mr-4 mr-0 sm:w-auto w-full sm:order-normal order-3 sm:mt-0 mt-4" v-model="searchQuery" @input="updateSearchQuery" placeholder="Search..." />
-        <!-- <vs-button class="mb-4 md:mb-0" @click="gridApi.exportDataAsCsv()">Export as CSV</vs-button> -->
-
-        <!-- ACTION - DROPDOWN -->
-        <vs-dropdown vs-trigger-click class="cursor-pointer">
-
-          <div class="p-3 shadow-drop rounded-lg d-theme-dark-light-bg cursor-pointer flex items-end justify-center text-lg font-medium w-32">
-            <span class="mr-2 leading-none">Actions</span>
-            <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
-          </div>
-
-          <vs-dropdown-menu>
-
-            <vs-dropdown-item>
-                <span class="flex items-center">
-                  <feather-icon icon="TrashIcon" svgClasses="h-4 w-4" class="mr-2" />
-                  <span>Delete</span>
-                </span>
-            </vs-dropdown-item>
-
-            <vs-dropdown-item>
-                <span class="flex items-center">
-                  <feather-icon icon="ArchiveIcon" svgClasses="h-4 w-4" class="mr-2" />
-                  <span>Archive</span>
-                </span>
-            </vs-dropdown-item>
-
-            <vs-dropdown-item>
-                <span class="flex items-center">
-                  <feather-icon icon="FileIcon" svgClasses="h-4 w-4" class="mr-2" />
-                  <span>Print</span>
-                </span>
-            </vs-dropdown-item>
-
-            <vs-dropdown-item>
-                <span class="flex items-center">
-                  <feather-icon icon="SaveIcon" svgClasses="h-4 w-4" class="mr-2" />
-                  <span>CSV</span>
-                </span>
-            </vs-dropdown-item>
-
-          </vs-dropdown-menu>
-        </vs-dropdown>
+        <vs-input class="sm:mr-4 mr-0 sm:w-auto w-full sm:order-normal order-3 sm:mt-0 mt-4" v-model="searchQuery"
+                  @input="updateSearchQuery" placeholder="Search..."/>
+        <vs-button class="mb-4 md:mb-0" @click="gridApi.exportDataAsCsv()">Export as CSV</vs-button>
       </div>
-
 
       <!-- AgGrid Table -->
       <ag-grid-vue
@@ -110,219 +59,130 @@
       <vs-pagination
         :total="totalPages"
         :max="7"
-        v-model="currentPage" />
-
+        v-model="currentPage"/>
     </div>
   </div>
 
 </template>
 
 <script>
-import { AgGridVue } from 'ag-grid-vue'
-import '@/assets/scss/vuexy/extraComponents/agGridStyleOverride.scss'
-import vSelect from 'vue-select'
+  import {AgGridVue} from 'ag-grid-vue'
+  import '@/assets/scss/vuexy/extraComponents/agGridStyleOverride.scss'
+  import vSelect from 'vue-select'
 
-// Store Module
+  import CellRendererStatus from './cell-renderer/CellRendererStatus.vue'
+  import CellRendererActions from './cell-renderer/CellRendererActions.vue'
 
-// Cell Renderer
-import CellRendererLink from './cell-renderer/CellRendererLink.vue'
-import CellRendererStatus from './cell-renderer/CellRendererStatus.vue'
-import CellRendererVerified from './cell-renderer/CellRendererVerified.vue'
-import CellRendererActions from './cell-renderer/CellRendererActions.vue'
-
-
-export default {
-  layout: "main",
-  components: {
-    AgGridVue,
-    vSelect,
-
-    // Cell Renderer
-    CellRendererLink,
-    CellRendererStatus,
-    CellRendererVerified,
-    CellRendererActions
-  },
-  data () {
-    return {
-      moduleName: "products",
-      // Filter Options
-      roleFilter: { label: 'All', value: 'all' },
-      roleOptions: [
-        { label: 'All', value: 'all' },
-        { label: 'Admin', value: 'admin' },
-        { label: 'User', value: 'user' },
-        { label: 'Staff', value: 'staff' }
-      ],
-
-      statusFilter: { label: 'All', value: 'all' },
-      statusOptions: [
-        { label: 'All', value: 'all' },
-        { label: 'Active', value: 'active' },
-        { label: 'Deactivated', value: 'deactivated' },
-        { label: 'Blocked', value: 'blocked' }
-      ],
-
-      isVerifiedFilter: { label: 'All', value: 'all' },
-      isVerifiedOptions: [
-        { label: 'All', value: 'all' },
-        { label: 'Yes', value: 'yes' },
-        { label: 'No', value: 'no' }
-      ],
-
-      departmentFilter: { label: 'All', value: 'all' },
-      departmentOptions: [
-        { label: 'All', value: 'all' },
-        { label: 'Sales', value: 'sales' },
-        { label: 'Development', value: 'development' },
-        { label: 'Management', value: 'management' }
-      ],
-
-      searchQuery: '',
-
-      // AgGrid
-      gridApi: null,
-      gridOptions: {},
-      defaultColDef: {
-        sortable: true,
-        resizable: true,
-        suppressMenu: true
-      },
-      columnDefs: [
-        {
-          headerName: 'Image',
-          field: 'image',
-          filter: true,
+  export default {
+    layout: "main",
+    components: {
+      AgGridVue,
+      vSelect,
+      CellRendererStatus,
+      CellRendererActions
+    },
+    data() {
+      return {
+        moduleName: "products",
+        searchQuery: '',
+        // AgGrid
+        gridApi: null,
+        gridOptions: {},
+        defaultColDef: {
+          sortable: true,
+          resizable: true,
+          suppressMenu: true
         },
-        {
-          headerName: 'Name',
-          field: 'name',
-          filter: true
-        },
-        {
-          headerName: 'Category',
-          field: 'category[0]',
-          filter: true,
-        },
-        {
-          headerName: 'Status',
-          field: 'status.description',
-          filter: true,
-          cellRendererFramework: 'CellRendererStatus'
-        },
-        {
-          headerName: 'Actions',
-          field: 'transactions',
-          cellRendererFramework: 'CellRendererActions'
+        columnDefs: [
+          {
+            headerName: 'Image',
+            field: 'image',
+            filter: true,
+          },
+          {
+            headerName: 'Name',
+            field: 'name',
+            filter: true
+          },
+          {
+            headerName: 'Category',
+            field: 'category[0]',
+            filter: true,
+          },
+          {
+            headerName: 'Status',
+            field: 'status.description',
+            filter: true,
+            cellRendererFramework: 'CellRendererStatus'
+          },
+          {
+            headerName: 'Actions',
+            field: 'transactions',
+            cellRendererFramework: 'CellRendererActions'
+          }
+        ],
+        components: {
+          CellRendererStatus,
+          CellRendererActions
         }
-      ],
-
-      // Cell Renderer Components
-      components: {
-        CellRendererLink,
-        CellRendererStatus,
-        CellRendererVerified,
-        CellRendererActions
       }
-    }
-  },
-  watch: {
-    roleFilter (obj) {
-      this.setColumnFilter('role', obj.value)
     },
-    statusFilter (obj) {
-      this.setColumnFilter('status', obj.value)
-    },
-    isVerifiedFilter (obj) {
-      const val = obj.value === 'all' ? 'all' : obj.value === 'yes' ? 'true' : 'false'
-      this.setColumnFilter('is_verified', val)
-    },
-    departmentFilter (obj) {
-      this.setColumnFilter('department', obj.value)
-    }
-  },
-  computed: {
-    records() {
-      return this.$store.state[this.moduleName].records;
-    },
-    paginationPageSize () {
-      if (this.gridApi) return this.gridApi.paginationGetPageSize()
-      else return 10
-    },
-    totalPages () {
-      if (this.gridApi) return this.gridApi.paginationGetTotalPages()
-      else return 0
-    },
-    currentPage: {
-      get () {
-        if (this.gridApi) return this.gridApi.paginationGetCurrentPage() + 1
-        else return 1
+    computed: {
+      records() {
+        return this.$store.state[this.moduleName].records;
       },
-      set (val) {
-        this.gridApi.paginationGoToPage(val - 1)
+      paginationPageSize() {
+        if (this.gridApi) return this.gridApi.paginationGetPageSize()
+        else return 10
+      },
+      totalPages() {
+        if (this.gridApi) return this.gridApi.paginationGetTotalPages()
+        else return 0
+      },
+      currentPage: {
+        get() {
+          if (this.gridApi) return this.gridApi.paginationGetCurrentPage() + 1
+          else return 1
+        },
+        set(val) {
+          this.gridApi.paginationGoToPage(val - 1)
+        }
       }
-    }
-  },
-  created(){
-    this.fetchItems();
-  },
-  methods: {
-    fetchItems(){
-      return this.$store.dispatch(this.moduleName + "/fetchItems");
-
     },
-    setColumnFilter (column, val) {
-      const filter = this.gridApi.getFilterInstance(column)
-      let modelObj = null
+    created() {
+      this.fetchItems();
+    },
+    methods: {
+      fetchItems() {
+        return this.$store.dispatch(this.moduleName + "/fetchItems");
 
-      if (val !== 'all') {
-        modelObj = { type: 'equals', filter: val }
+      },
+      updateSearchQuery(val) {
+        this.gridApi.setQuickFilter(val)
       }
-
-      filter.setModel(modelObj)
-      this.gridApi.onFilterChanged()
     },
-    resetColFilters () {
-      // Reset Grid Filter
-      this.gridApi.setFilterModel(null)
-      this.gridApi.onFilterChanged()
+    mounted() {
+      this.gridApi = this.gridOptions.api
+      this.gridApi.sizeColumnsToFit()
 
-      // Reset Filter Options
-      this.roleFilter = this.statusFilter = this.isVerifiedFilter = this.departmentFilter = { label: 'All', value: 'all' }
-
-      this.$refs.filterCard.removeRefreshAnimation()
+      if (this.$vs.rtl) {
+        const header = this.$refs.agGridTable.$el.querySelector('.ag-header-container')
+        header.style.left = `-${String(Number(header.style.transform.slice(11, -3)) + 9)}px`
+      }
     },
-    updateSearchQuery (val) {
-      this.gridApi.setQuickFilter(val)
-    }
-  },
-  mounted () {
-    this.gridApi = this.gridOptions.api
-    this.gridApi.sizeColumnsToFit()
-    /* =================================================================
-      NOTE:
-      Header is not aligned properly in RTL version of agGrid table.
-      However, we given fix to this issue. If you want more robust solution please contact them at gitHub
-    ================================================================= */
-    if (this.$vs.rtl) {
-      const header = this.$refs.agGridTable.$el.querySelector('.ag-header-container')
-      header.style.left = `-${  String(Number(header.style.transform.slice(11, -3)) + 9)  }px`
-    }
-  },
-
-}
+  }
 
 </script>
 
 <style lang="scss">
-#page-user-list {
-  .user-list-filters {
-    .vs__actions {
-      position: absolute;
-      right: 0;
-      top: 50%;
-      transform: translateY(-58%);
+  #page-user-list {
+    .user-list-filters {
+      .vs__actions {
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-58%);
+      }
     }
   }
-}
 </style>
