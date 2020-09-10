ffdc304af772a5c874977b448147eaec5e846e5c
Farhan <ahmad.farhan@globaltium.com>
2020-09-10 11:35:45 +0800

    Update dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -98,14 +98,14 @@
               <div class="mr-6">
                 <p class="mb-1 font-semibold">This Month</p>
                 <p class="text-3xl text-success">
-                  <sup class="text-base mr-1">$</sup>
+                  <sup class="text-base mr-1">RM</sup>
                   {{ salesChart.analyticsData.thisMonth.toLocaleString() }}
                 </p>
               </div>
               <div>
                 <p class="mb-1 font-semibold">Last Month</p>
                 <p class="text-3xl">
-                  <sup class="text-base mr-1">$</sup>
+                  <sup class="text-base mr-1">RM</sup>
                   {{ salesChart.analyticsData.lastMonth.toLocaleString() }}
                 </p>
               </div>
