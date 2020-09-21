3325083e3dce25b9f5f7c1596b750bc45a2d37ed
Farhan <ahmad.farhan@globaltium.com>
2020-09-22 06:33:39 +0800

    Add PROXY_SECURE env variable

--- config/nuxt/proxy.js
+++ config/nuxt/proxy.js
@@ -1,12 +1,13 @@
-require("dotenv").config();
+require('dotenv').config();
 
 export default {
-  "/v1": {
-    target: process.env.API_URL,
-    pathRewrite: {
-      "^/v1": "/v1"
-    }
-  }
+	'/v1': {
+		target: process.env.API_URL,
+		pathRewrite: {
+			'^/v1': '/v1'
+		},
+		secure: process.env.PROXY_SECURE || false
+	}
 };
 
 // export default ["http://kolstore-api.test/v1"];
