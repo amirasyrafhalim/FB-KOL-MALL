ebaf3a43278af3fcdbbef06acbec086adbfe61c1
naja <naja.nadhirah@ionnex.com>
2020-09-22 16:52:45 +0800

    fix: update payment status bug

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -97,12 +97,12 @@ export default {
 		getAllOrCreate: '/v1/buyers',
 		getOneOrUpdateOrDelete: '/v1/buyers/{id}'
 	},
-	shippingPartners: {
-		getAll: '/v1/shipping-partners'
-	},
+	// shippingPartners: {
+	// 	getAll: '/v1/shipping-partners'
+	// },
 	orders: {
-		updateOrderPayment: '/v1/order-payments/{id}',
 		getAllOrCreate: '/v1/orders',
+		updateOrderPayment: '/v1/order-payments/{id}',
 		getOneOrUpdateOrDelete: '/v1/orders/{id}',
 		updateDelivery: '/v1/order-deliveries/{id}',
 		updatePickup: '/v1/orders/{id}/updatePickup'
@@ -132,37 +132,37 @@ export default {
 		getAllOrCreate: '/v1/videos',
 		getOneOrUpdateOrDelete: '/v1/videos/{id}'
 	},
-	dashboard: {
-		getSalesSummary: '/v1/dashboard/sales-summary',
-		getSalesChart: '/v1/dashboard/sales-chart'
-	},
+	// dashboard: {
+	// 	getSalesSummary: '/v1/dashboard/sales-summary',
+	// 	getSalesChart: '/v1/dashboard/sales-chart'
+	// },
 	categories: {
 		getAllOrCreate: '/v1/categories',
 		getOneOrUpdateOrDelete: '/v1/campaigns/{id}'
 	},
-	campaignPackages: {
-		getAllOrCreate: '/v1/packages',
-		getOneOrUpdateOrDelete: '/v1/packages/{id}'
-	},
-	campaignShippings: {
-		getAllOrCreate: '/v1/package-shipping',
-		getOneOrUpdateOrDelete: '/v1/campaigns/{campaignId}/campaignShippings/{id}'
-	},
-	merchants: {
-		getAllOrCreate: '/v1/merchants',
-		getDetail: '/v1/merchants/detail',
-		getOneOrUpdateOrDelete: '/v1/merchants/{id}'
-	},
-	merchantPages: {
-		getAllOrCreate: '/v1/merchant-pages',
-		getOneOrUpdateOrDelete: '/v1/merchant-pages/{id}',
-		liveVideos: '/v1/merchant-pages/{id}/live-videos'
-	},
-	merchantPageTokens: {
-		getAllOrCreate: '/v1/merchantPageTokens',
-		getOneOrUpdateOrDelete: '/v1/merchantPageTokens/{id}',
-		debugPageToken: '/v1/merchantPageTokens/{id}/debugMerchantPageToken'
-	},
+	// campaignPackages: {
+	// 	getAllOrCreate: '/v1/packages',
+	// 	getOneOrUpdateOrDelete: '/v1/packages/{id}'
+	// },
+	// campaignShippings: {
+	// 	getAllOrCreate: '/v1/package-shipping',
+	// 	getOneOrUpdateOrDelete: '/v1/campaigns/{campaignId}/campaignShippings/{id}'
+	// },
+	// merchants: {
+	// 	getAllOrCreate: '/v1/merchants',
+	// 	getDetail: '/v1/merchants/detail',
+	// 	getOneOrUpdateOrDelete: '/v1/merchants/{id}'
+	// },
+	// merchantPages: {
+	// 	getAllOrCreate: '/v1/merchant-pages',
+	// 	getOneOrUpdateOrDelete: '/v1/merchant-pages/{id}',
+	// 	liveVideos: '/v1/merchant-pages/{id}/live-videos'
+	// },
+	// merchantPageTokens: {
+	// 	getAllOrCreate: '/v1/merchantPageTokens',
+	// 	getOneOrUpdateOrDelete: '/v1/merchantPageTokens/{id}',
+	// 	debugPageToken: '/v1/merchantPageTokens/{id}/debugMerchantPageToken'
+	// },
 	shippingMethods: {
 		getAllOrCreate: '/v1/shipping-methods',
 		getOneOrUpdateOrDelete: '/v1/shipping-methods/{id}'
@@ -171,54 +171,54 @@ export default {
 		getAllOrCreate: '/v1/shipping-partners',
 		getOneOrUpdateOrDelete: '/v1/shipping-partners/{id}'
 	},
-	products: {
-		getAllOrCreate: '/v1/products',
-		getOneOrUpdateOrDelete: '/v1/products/{id}'
-	},
-	merchantShippings: {
-		getAllOrCreate: '/v1/merchantShippings',
-		getOneOrUpdateOrDelete: '/v1/merchantShippings/{id}'
-	},
-	dropdown: {
-		getAllCountry: '/v1/dropdown/country',
-		getAllPostcode: '/v1/dropdown/postcode',
-		getAllState: '/v1/dropdown/state'
-	},
-	buyers: {
-		getAllOrCreate: '/v1/buyers',
-		getOneOrUpdateOrDelete: '/v1/buyers/{id}'
-	},
-	orders: {
-		getAllOrCreate: '/v1/orders',
-		getOneOrUpdateOrDelete: '/v1/orders/{id}',
-		updateDelivery: '/v1/order-deliveries/{id}',
-		updatePickup: '/v1/orders/{id}/updatePickup'
-	},
-	orderDeliveries: {
-		updateOrderDelivery: '/v1/order-deliveries/{id}'
-	},
-	facebook: {
-		video: '/v1/facebook/addVideo'
-	},
-	buyerOrders: {
-		addBuyerOrder: '/v1/buyerOrder'
-	},
-	password: {
-		resetPassword: '/v1/password/reset-request',
-		newPassword: '/v1/password/reset',
-		updatePassword: '/v1/update-passwords'
-	},
-	user: {
-		getAllOrCreate: '/v1/users',
-		getOneOrUpdateOrDelete: '/v1/users/{id}'
-	},
-	email: {
-		verifiedEmail: '/v1/email/verify'
-	},
-	videos: {
-		getAllOrCreate: '/v1/videos',
-		getOneOrUpdateOrDelete: '/v1/videos/{id}'
-	},
+	// products: {
+	// 	getAllOrCreate: '/v1/products',
+	// 	getOneOrUpdateOrDelete: '/v1/products/{id}'
+	// },
+	// merchantShippings: {
+	// 	getAllOrCreate: '/v1/merchantShippings',
+	// 	getOneOrUpdateOrDelete: '/v1/merchantShippings/{id}'
+	// },
+	// dropdown: {
+	// 	getAllCountry: '/v1/dropdown/country',
+	// 	getAllPostcode: '/v1/dropdown/postcode',
+	// 	getAllState: '/v1/dropdown/state'
+	// },
+	// buyers: {
+	// 	getAllOrCreate: '/v1/buyers',
+	// 	getOneOrUpdateOrDelete: '/v1/buyers/{id}'
+	// },
+	// orders: {
+	// 	getAllOrCreate: '/v1/orders',
+	// 	getOneOrUpdateOrDelete: '/v1/orders/{id}',
+	// 	updateDelivery: '/v1/order-deliveries/{id}',
+	// 	updatePickup: '/v1/orders/{id}/updatePickup'
+	// },
+	// orderDeliveries: {
+	// 	updateOrderDelivery: '/v1/order-deliveries/{id}'
+	// },
+	// facebook: {
+	// 	video: '/v1/facebook/addVideo'
+	// },
+	// buyerOrders: {
+	// 	addBuyerOrder: '/v1/buyerOrder'
+	// },
+	// password: {
+	// 	resetPassword: '/v1/password/reset-request',
+	// 	newPassword: '/v1/password/reset',
+	// 	updatePassword: '/v1/update-passwords'
+	// },
+	// user: {
+	// 	getAllOrCreate: '/v1/users',
+	// 	getOneOrUpdateOrDelete: '/v1/users/{id}'
+	// },
+	// email: {
+	// 	verifiedEmail: '/v1/email/verify'
+	// },
+	// videos: {
+	// 	getAllOrCreate: '/v1/videos',
+	// 	getOneOrUpdateOrDelete: '/v1/videos/{id}'
+	// },
 	dashboard: {
 		getSalesSummary: '/v1/dashboard/sales-summary',
 		getSalesChart: '/v1/dashboard/sales-chart',
