28d868925698b646cf2a4e0e189e57239c001282
KN <khairunnashriah.mohd@inx.my>
2020-09-25 11:16:20 +0800

    Feat : re-add and re-design reset password pages

--- pages/newPasswordForm.vue
+++ pages/newPasswordForm.vue
@@ -1,63 +1,66 @@
 <template>
-  <div>
-    <div class="fill-height h-screen flex w-full bg-img vx-row no-gutter items-center justify-center">
-    <v-layout>
-      <v-row align="center" justify="center">
-        <v-col cols="12" sm="8" md="5">
-          <v-card class="elevation-1 pa-3">
-            <alert-form-error :error-message="errorMessage" />
-            <v-card-title>
-              <v-btn :to="localePath('login')" icon small>
-                <v-icon>mdi-keyboard-backspace</v-icon>
-              </v-btn>
-              <div><p class="text-center my-auto">RESET PASSWORD</p></div>
-            </v-card-title>
-            <v-divider> </v-divider>
-            <v-card-text>
-              <v-form>
-                <v-text-field
-                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                  :rules="[rules.password]"
-                  :type="show1 ? 'text' : 'password'"
-                  label="New Password"
-                  hint="At least 8 characters"
-                  v-model="formModel.password"
-                  class="input-group--focused"
-                  :error-messages="formErrors ? formErrors.password : ''"
-                  @click:append="show1 = !show1"
-                ></v-text-field>
-
-                <v-text-field
-                  :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                  :rules="[rules.password, passwordConfirmationRule]"
-                  :type="show2 ? 'text' : 'password'"
-                  label="New Password"
-                  hint="At least 8 characters"
-                  v-model="formModel.password_confirmation"
-                  class="input-group--focused"
-                  :error-messages="
-                    formErrors ? formErrors.password_comfirmation : ''
-                  "
-                  @click:append="show2 = !show2"
-                ></v-text-field>
-
-                <v-btn
-                  block
-                  color="primary"
-                  class="mr-4 "
-                  @click="submit"
-                  :loading="loading"
-                >
-                  SUBMIT
-                </v-btn>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-layout>
+  <div class="h-screen flex w-full bg-img">
+        <div class="vx-col w-4/5 sm:w-4/5 md:w-3/5 lg:w-3/4 xl:w-3/5 mx-auto self-center">
+            <vx-card>
+                <div slot="no-body" class="full-page-bg-color">
+                    <div class="vx-row">
+                        <div class="vx-col hidden sm:hidden md:hidden lg:block lg:w-1/2 mx-auto self-center">
+                            <img src="@/assets/images/pages/forgot-password.png" alt="login" class="mx-auto">
+                        </div>
+                        <div class="vx-col sm:w-full md:w-full lg:w-1/2 mx-auto self-center d-theme-dark-bg">
+                            <div class="p-8">
+                                <alert-form-error :error-message="errorMessage" />
+                                <div class="vx-card__title mb-8">
+                                    <h4 class="mb-4">Reset Password</h4>
+                                    <p>Please enter your new password.</p>
+                                </div>
+                                <vs-input
+                                  placeholder="New Password"
+                                  icon-no-border
+                                  icon-pack="feather"
+                                  icon-after="true"
+                                  :icon="show1 ? 'icon icon-eye' : 'icon icon-eye-off'"
+                                  :type="show1 ? 'password' : 'text'"
+                                  :rules="[rules.password]"
+                                  :error-messages="formErrors ? formErrors.password : ''"
+                                  hint="At least 8 characters"
+                                  v-model="formModel.password"
+                                  v-on:icon-click="show1 = !show1"
+                                  class="w-full mb-8" />
+                                <vs-input
+                                  placeholder="Confirm New Password"
+                                  icon-no-border
+                                  icon-pack="feather"
+                                  icon-after="true"
+                                  :icon="show2 ? 'icon icon-eye' : 'icon icon-eye-off'"
+                                  :type="show2 ? 'password' : 'text'"
+                                  :rules="[rules.password, passwordConfirmationRule]"
+                                  :error-messages="
+                                    formErrors ? formErrors.password_comfirmation : ''
+                                  "
+                                  hint="At least 8 characters"
+                                  v-model="formModel.password_confirmation"
+                                  v-on:icon-click="show2 = !show2"
+                                  class="w-full mb-8" />
+                                <vs-button
+                                  type="border"
+                                  to="/login"
+                                  class="px-4 w-full md:w-auto">
+                                  Back To Login
+                                  </vs-button>
+                                <vs-button 
+                                  class="float-right px-4 w-full md:w-auto mt-3 mb-8 md:mt-0 md:mb-0"
+                                  @click="submit"
+                                  :loading="loading">
+                                  Submit
+                                </vs-button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </vx-card>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
