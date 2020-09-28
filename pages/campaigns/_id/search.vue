920f0e19749b7e681a35afe6a6d0f87076a1a3f2
nadiahsohaini <nadiahsohaini@gmail.com>
2020-09-28 12:54:13 +0800

    feat: filter in shippng method, package, campaign and video

---
+++ pages/campaigns/_id/search.vue
@@ -0,0 +1,78 @@
+<template>
+  <vx-card ref="filterCard" title="Filters" class="user-list-filters mb-8" actionButtons @refresh="resetItem"
+           @remove="resetItem">
+    <div class="vx-row">
+      <div class="vx-col md:w-1/4 sm:w-1/2 w-full">
+        <label class="text-sm opacity-75">Name</label>
+        <vs-input class="w-full mb-4 md:mb-0" v-model="dataName"/>
+      </div>
+      <div class="vx-col md:w-1/4 sm:w-1/2 w-full">
+        <label class="text-sm opacity-75">Keyword</label>
+        <vs-input class="w-full mb-4 md:mb-0" v-model="dataKeyword"/>
+      </div>
+      <div class="vx-col md:w-1/4 sm:w-1/2 w-full">
+        <label class="text-sm opacity-75">Status</label>
+        <v-select :options="activeStatus" label="description" :clearable=false v-model="dataStatus" class="mb-4 md:mb-0"/>
+      </div>
+      <div class="vx-col md:w-1/4 sm:w-1/2 w-full" style="padding-top: 19px">
+        <vs-button color="primary" @click="searchItem()">Search</vs-button>
+      </div>
+    </div>
+  </vx-card>
+</template>
+
+<script>
+  export default {
+    name: "FormSearch",
+    data() {
+      return {
+        moduleName: "packages",
+        dataName: '',
+        dataStatus: null,
+        dataKeyword: ''
+      };
+    },
+    created() {
+      this.initialize();
+    },
+    computed: {
+      activeStatus() {
+        return this.$store.state[this.moduleName].activeStatus;
+      },
+    },
+    methods: {
+      initialize() {
+        this.$store.dispatch(`${this.moduleName}/fetchEnums`, {
+          routeName: "activeStatus",
+          stateKey: "activeStatus",
+        });
+      },
+      async searchItem() {
+        var item = {};
+
+        item.name = this.dataName;
+        item.keyword = this.dataKeyword;
+        if(this.dataStatus) {
+          item.status = this.dataStatus.value;
+        }
+        await this.$store.dispatch(this.moduleName + "/fetchItems", item);
+        await this.$store.dispatch(this.moduleName + "/resetPagination");
+      },
+      async resetItem() {
+        this.dataName = '';
+        this.dataStatus = null;
+        this.dataKeyword = '';
+
+        this.$refs.filterCard.removeRefreshAnimation()
+        await this.$store.dispatch(this.moduleName + "/fetchItems", {campaign_id: this.$route.params.id});
+      },
+    },
+  };
+</script>
+
+<style>
+  .feather-x {
+    display: none !important;
+  }
+</style>
+
--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -1,55 +1,57 @@
 <template>
   <div id="data-list-list-view" class="data-list-container">
-    <vs-table
-      ref="table"
-      v-model="selected"
-      pagination
-      :max-items="itemsPerPage"
-      search
-      :data="records"
-      class="bg-transparent"
-    >
-      <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between">
-        <vs-dropdown
-          vs-trigger-click
-          class="cursor-pointer mb-4 mr-4 items-per-page-handler ml-auto"
-        >
-          <div
-            class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium"
+    <search></search>
+
+    <div class="vx-card">
+      <vs-table
+        stripe
+        ref="table"
+        v-model="selected"
+        pagination
+        :max-items="itemsPerPage"
+        :data="records"
+        class="bg-transparent"
+      >
+        <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between p-6">
+          <vs-dropdown
+            vs-trigger-click
+            class="cursor-pointer items-per-page-handler ml-auto"
           >
+            <div
+              class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium"
+            >
             <span
               class="mr-2"
             >{{ currentPage * itemsPerPage - (itemsPerPage - 1) }} - {{ records.length - currentPage * itemsPerPage > 0 ? currentPage * itemsPerPage : records.length }} of {{ queriedItems }}</span>
-            <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
-          </div>
-          <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
-          <vs-dropdown-menu>
-            <vs-dropdown-item @click="itemsPerPage=10">
-              <span>10</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=20">
-              <span>20</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=30">
-              <span>30</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=40">
-              <span>40</span>
-            </vs-dropdown-item>
-          </vs-dropdown-menu>
-        </vs-dropdown>
-      </div>
-      <template slot="thead">
-        <vs-th sort-key="stream_id">ID</vs-th>
-        <vs-th sort-key="stream_video_id">Video ID</vs-th>
-        <vs-th sort-key="title">Title</vs-th>
-        <vs-th sort-key="start_at">Broadcast At</vs-th>
-        <vs-th sort-key="created_at">Created At</vs-th>
-        <vs-th>Action</vs-th>
-      </template>
+              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4"/>
+            </div>
+            <vs-dropdown-menu>
+              <vs-dropdown-item @click="itemsPerPage=10">
+                <span>10</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=20">
+                <span>20</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=30">
+                <span>30</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="itemsPerPage=40">
+                <span>40</span>
+              </vs-dropdown-item>
+            </vs-dropdown-menu>
+          </vs-dropdown>
+        </div>
+        <template slot="thead">
+          <vs-th sort-key="stream_id">ID</vs-th>
+          <vs-th sort-key="stream_video_id">Video ID</vs-th>
+          <vs-th sort-key="title">Title</vs-th>
+          <vs-th sort-key="start_at">Broadcast At</vs-th>
+          <vs-th sort-key="created_at">Created At</vs-th>
+          <vs-th>Action</vs-th>
+        </template>
 
-      <template slot-scope="{data}">
-        <tbody>
+        <template slot-scope="{data}">
+          <tbody>
           <vs-tr :data="tr" :key="indextr" v-for="(tr, indextr) in data">
             <vs-td :data="data[indextr].stream_id">{{ tr.stream_id }}</vs-td>
             <vs-td :data="data[indextr].stream_video_id">{{ tr.stream_video_id }}</vs-td>
@@ -57,186 +59,75 @@
             <vs-td :data="data[indextr].start_at">{{ tr.start_at }}</vs-td>
             <vs-td :data="data[indextr].created_at">{{ tr.created_at }}</vs-td>
             <vs-td class="whitespace-no-wrap">
-              <nuxt-link :to="localePath({ name: 'videos-id', params: { id: tr.id } })">
-                <feather-icon
-                  icon="VideoIcon"
-                  svgClasses="w-5 h-5 hover:text-primary stroke-current"
-                />
-              </nuxt-link>
+              <vs-button color="primary" type="border" icon="videocam" class="ml-2" :to="localePath({ name: 'videos-id', params: { id: tr.id } })"></vs-button>
             </vs-td>
           </vs-tr>
-        </tbody>
-      </template>
-    </vs-table>
+          </tbody>
+        </template>
+      </vs-table>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  layout: "main",
-  data() {
-    return {
-      moduleName: "videos",
-      selected: [],
-      itemsPerPage: 10,
-      isMounted: false,
-      addNewDataSidebar: false,
-      sidebarData: {},
-    };
-  },
-  computed: {
-    currentPage() {
-      if (this.isMounted) {
-        return this.$refs.table.currentx;
-      }
-      return 0;
+  import search from './search'
+
+  export default {
+    layout: "main",
+    components: {
+      search
     },
-    records() {
-      return this.$store.state[this.moduleName].records;
+    data() {
+      return {
+        moduleName: "videos",
+        selected: [],
+        itemsPerPage: 10,
+        isMounted: false,
+        addNewDataSidebar: false,
+        sidebarData: {},
+      };
     },
-    queriedItems() {
-      return this.$refs.table
-        ? this.$refs.table.queriedResults.length
-        : this.records.length;
+    computed: {
+      currentPage() {
+        if (this.isMounted) {
+          return this.$refs.table.currentx;
+        }
+        return 0;
+      },
+      records() {
+        return this.$store.state[this.moduleName].records;
+      },
+      queriedItems() {
+        return this.$refs.table
+          ? this.$refs.table.queriedResults.length
+          : this.records.length;
+      },
     },
-  },
-  created() {
-    this.fetchItems();
-  },
-  mounted() {
-    this.isMounted = true;
-  },
-  methods: {
-    fetchItems(page = 1) {
-      let params = { page: page };
-      this.$store.dispatch(this.moduleName + "/fetchItems", params);
+    created() {
+      this.fetchItems();
     },
-  },
-};
+    mounted() {
+      this.isMounted = true;
+    },
+    methods: {
+      fetchItems(page = 1) {
+        let params = {page: page};
+        this.$store.dispatch(this.moduleName + "/fetchItems", params);
+      },
+    },
+  };
 </script>
 
 <style lang="scss">
-#data-list-list-view {
-  .vs-con-table {
-    @media (max-width: 689px) {
-      .vs-table--search {
-        margin-left: 0;
-        max-width: unset;
-        width: 100%;
-
-        .vs-table--search-input {
-          width: 100%;
-        }
-      }
+  #data-list-list-view {
+    .vs-lg-6 {
+      width: 100% !important;
     }
 
-    @media (max-width: 461px) {
-      .items-per-page-handler {
-        display: none;
+    .vs-con-table {
+      .vs-con-tbody {
+        border: none !important;
       }
-    }
-
-    @media (max-width: 341px) {
-      .data-list-btn-container {
-        width: 100%;
-
-        .dd-actions,
-        .btn-add-new {
-          width: 100%;
-          margin-right: 0 !important;
-        }
-      }
-    }
-
-    .product-name {
-      max-width: 23rem;
-    }
-
-    .vs-table--header {
-      display: flex;
-      flex-wrap: wrap;
-      margin-left: 1.5rem;
-      margin-right: 1.5rem;
-
-      > span {
-        display: flex;
-        flex-grow: 1;
-      }
-
-      .vs-table--search {
-        padding-top: 0;
-
-        .vs-table--search-input {
-          padding: 0.9rem 2.5rem;
-          font-size: 1rem;
-
-          & + i {
-            left: 1rem;
-          }
-
-          &:focus + i {
-            left: 1rem;
-          }
-        }
-      }
-    }
-
-    .vs-table {
-      border-collapse: separate;
-      border-spacing: 0 1.3rem;
-      padding: 0 1rem;
-
-      tr {
-        box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
-
-        td {
-          padding: 20px;
-
-          &:first-child {
-            border-top-left-radius: 0.5rem;
-            border-bottom-left-radius: 0.5rem;
-          }
-
-          &:last-child {
-            border-top-right-radius: 0.5rem;
-            border-bottom-right-radius: 0.5rem;
-          }
-        }
-
-        td.td-check {
-          padding: 20px !important;
-        }
-      }
-    }
-
-    .vs-table--thead {
-      th {
-        padding-top: 0;
-        padding-bottom: 0;
-
-        .vs-table-text {
-          text-transform: uppercase;
-          font-weight: 600;
-        }
-      }
-
-      th.td-check {
-        padding: 0 15px !important;
-      }
-
-      tr {
-        background: none;
-        box-shadow: none;
-      }
-    }
-
-    .vs-table--pagination {
-      justify-content: center;
     }
   }
-
-  .vs-lg-6 {
-    width: 100% !important;
-  }
-}
 </style>
